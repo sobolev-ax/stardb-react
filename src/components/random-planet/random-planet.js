479653321863906262b306717b2bc47d54357485
sobolev.ax <sobolev.ax@gmail.com>
2019-02-26 00:02:23 +0300

    Added alt atribute to img

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -47,6 +47,7 @@ export default class RandomPlanet extends Component {
     return (
       <div className="random-planet jumbotron rounded">
         <img className="planet-image"
+             alt={`${name} photo`}
              src={id ? `https://starwars-visualguide.com/assets/img/planets/${id}.jpg` : ''}/>
         <div>
           <h4>{name}</h4>

c6d7b84defa4b20d0974b1b861c3921915c722b2
Alexander Sobolev <sobolev.ax@gmail.com>
2019-04-02 14:56:39 +0300

    Added Context

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,16 +3,23 @@ import React, { Component } from 'react';
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import PeoplePage from '../people-page'
+import SwapiService from '../../services/swapi-service';
+import { SwapiServiceProvider } from '../swapi-service-context';
 
 import './app.css';
 
 class App extends Component {
+
+  swapiService = new SwapiService();
+
   render() {
     return (
       <div className="container">
-        <Header />
-        <RandomPlanet />
-        <PeoplePage />
+        <SwapiServiceProvider value={this.swapiService}>
+          <Header />
+          <RandomPlanet />
+          <PeoplePage />
+        </SwapiServiceProvider>
       </div>
     );
   }
--- src/components/sw-components/details.js
+++ src/components/sw-components/details.js
@@ -1,49 +1,65 @@
 import React from 'react';
 import ItemDetails, { Record } from '../item-details';
-import SwapiService from '../../services/swapi-service';
+import { SwapiServiceConsumer } from '../swapi-service-context';
 
-const {
-  getPerson,
-  getStarship,
-  getPlanet,
-  getImagePerson,
-  getImageStarship,
-  getImagePlanet,
-} = new SwapiService();
 
 
 
 const PersonDetails = ({ selectedItem }) => {
   return (
-    <ItemDetails selectedItem={selectedItem}
-                 getData={getPerson}
-                 getImage={getImagePerson}>
-      <Record label="Gender" field="gender" />
-      <Record label="Birth Year" field="birthYear" />
-      <Record label="Eye Color" field="EyeColor" />
-    </ItemDetails>
+    <SwapiServiceConsumer>
+      {
+        ({ getPerson, getImagePerson }) => {
+          return (
+            <ItemDetails selectedItem={selectedItem}
+                         getData={getPerson}
+                         getImage={getImagePerson}>
+              <Record label="Gender" field="gender" />
+              <Record label="Birth Year" field="birthYear" />
+              <Record label="Eye Color" field="EyeColor" />
+            </ItemDetails>
+          );
+        }
+      }
+    </SwapiServiceConsumer>
   );
 };
 const PlanetDetails = ({ selectedItem }) => {
   return (
-    <ItemDetails selectedItem={selectedItem}
-                 getData={getPlanet}
-                 getImage={getImagePlanet}>
-      <Record label="Diameter" field="diameter" />
-      <Record label="Population" field="population" />
-      <Record label="Period" field="rotationPeriod" />
-    </ItemDetails>
+    <SwapiServiceConsumer>
+      {
+        ({ getPlanet, getImagePlanet }) => {
+          return (
+            <ItemDetails selectedItem={selectedItem}
+                         getData={getPlanet}
+                         getImage={getImagePlanet}>
+              <Record label="Diameter" field="diameter" />
+              <Record label="Population" field="population" />
+              <Record label="Period" field="rotationPeriod" />
+            </ItemDetails>
+          );
+        }
+      }
+    </SwapiServiceConsumer>
   );
 };
 const StarshipDetails = ({ selectedItem }) => {
   return (
-    <ItemDetails selectedItem={selectedItem}
-                 getData={getStarship}
-                 getImage={getImageStarship}>
-      <Record label="Passengers" field="passengers" />
-      <Record label="Length" field="length" />
-      <Record label="Cost" field="costInCredits" />
-    </ItemDetails>
+    <SwapiServiceConsumer>
+      {
+        ({ getStarship, getImageStarship }) => {
+          return (
+            <ItemDetails selectedItem={selectedItem}
+                         getData={getStarship}
+                         getImage={getImageStarship}>
+            <Record label="Passengers" field="passengers" />
+            <Record label="Length" field="length" />
+            <Record label="Cost" field="costInCredits" />
+            </ItemDetails>
+          );
+        }
+      }
+    </SwapiServiceConsumer>
   );
 };
 
